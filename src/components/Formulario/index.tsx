187d3ea00c9ee7338656c7de071cbf26121d7a0d
Felipe Alexandre Duarte Araújo Ramos <felipeadar108@gmail.com>
2023-11-16 14:51:26 -0300

    :sparkles: feat: função de adicionar tarefas funcionando

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -3,9 +3,18 @@ import Botao from "../Button";
 import style from "./Formulario.module.scss";
 
 class Formulario extends React.Component {
+    state= {
+        tarefa: "",
+        tempo: "00:00",
+    };
+
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>){
+        evento.preventDefault();
+        console.log(this.state);
+    }
     render() {
         return (
-            <form className={style.novaTarefa}> 
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}> 
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo:
@@ -13,6 +22,12 @@ class Formulario extends React.Component {
                         type="text"
                         name="tarefa"
                         id="tarefa"
+                        value={this.state.tarefa}
+                        onChange={(evento) => {
+                            this.setState(
+                                {...this.state, tarefa: evento.target.value}
+                            )
+                        }}
                         placeholder="O que você quer estudar?" 
                         required />
                     </label>
@@ -23,13 +38,21 @@ class Formulario extends React.Component {
                         <input type="time"
                         step="1"
                         name="tempo"
+                        value={this.state.tempo}
+                        onChange={(evento) => {
+                            this.setState({
+                                ...this.state, tempo: evento.target.value
+                            })
+                        }}
                         id="tempo"
                         min="00:00:00"
                         max="01:30:00"
                         required />
                     </label>
                 </div>
-            < Botao />
+            <Botao>
+                Adicionar
+            </Botao>
             </form>
         )
     }
